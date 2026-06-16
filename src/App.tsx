13d4e8580ae980f6faaa463fb49eedb0ce68940f
Anderson <anderson@solomon.com>
2026-06-15 23:30:20 -0300

    Fix unused imports to unblock vercel build

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useRef, useState, useEffect } from 'react';
+import { useRef } from 'react';
 
 import { Hero } from './components/Hero/Hero';
 import { FloatingWhatsApp } from './components/FloatingWhatsApp/FloatingWhatsApp';
